5ebba70ba15c368a91fdef8df3e932e94f0e9268
neeraj-kumar-yadav <166293215+neeraj-kumar-yadav@users.noreply.github.com>
2025-03-18 18:00:57 +0530

    Update bleu_score.py

--- bleu_score.py
+++ bleu_score.py
@@ -24,8 +24,6 @@ def load_scores():
 # Save BLEU scores to file
 def save_scores(query, bleu_score):
     existing_scores = load_scores()
-    
-    # Store only BLEU scores greater than 0.65
     new_entry = {
         "query": query,
         "bleu_score": bleu_score
@@ -69,6 +67,10 @@ def plot_bleu_scores():
     plt.xlabel("Query Number", fontsize=12, labelpad=10)  # Increase label font size & padding
     plt.ylabel("BLEU Score", fontsize=12, labelpad=10)
     plt.title("Chatbot BLEU Score", fontsize=13)
+
+    plt.ylim(0, max(scores, default=1) + 0.2)
+    plt.legend(fontsize=10)
+    plt.grid()
 
     plt.tight_layout()  # Prevents label cropping
     plt.savefig("bleu_score_plot.png", dpi=600, bbox_inches='tight')
